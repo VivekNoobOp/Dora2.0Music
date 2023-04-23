318414d4c67fe586b853f34e1efcd59982594e64
PiroKido <129570257+PiroKido@users.noreply.github.com>
2023-04-24 01:43:24 +0530

    Update __init__.py

--- FallenMusic/__init__.py
+++ FallenMusic/__init__.py
@@ -92,8 +92,8 @@ async def fallen_startup():
     ASS_USERNAME = getme2.username
     ASS_MENTION = getme2.mention
     try:
-        await app2.join_chat("DevilsHeavenMF")
-        await app2.join_chat("FallenAssociation")
+        await app2.join_chat("NoobCreator")
+        await app2.join_chat("NoobXCreator")
     except:
         pass
 
